d2a6299eaed584c0ae956be39922adf4028cd3ca
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2022-08-10 16:42:51 +0900

    22_08_10

---
+++ baekjoon/baek24444.cpp
@@ -0,0 +1,65 @@
+//24444번 알고리즘 수업 너비우선탐색 1
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+vector<int> *graph =new vector<int>[100001];
+int isVisited[100001] = {0,};
+int cnt = 0;
+queue<int> q;
+bool cmp(int x, int y){
+    if(x>y){
+        return true;
+    }else
+        return false;
+}
+void dfs(int current){
+    if(isVisited[current] != 0){
+        return;
+    }
+    cnt++;
+    isVisited[current] = cnt;
+    for(int i = 0; i< graph[current].size(); i++){
+        if(isVisited[graph[current][i]]==0)
+            dfs(graph[current][i]);
+    }
+}
+void bfs(int current){
+    cnt++;
+    isVisited[current] = cnt;
+    q.push(current);
+    while(!q.empty()){
+        int next = q.front();
+        q.pop();
+        for(int i = 0; i<graph[next].size();i++){
+            if(isVisited[graph[next][i]]==0){
+                cnt++;
+                isVisited[graph[next][i]]=cnt;
+                q.push(graph[next][i]);
+            }
+        }
+    }
+
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int n, m, r;
+    cin >> n >> m >> r;
+    for(int i = 0; i < m; i++){
+        int u, v;
+        cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+    for(int i = 0; i<=n; i++){
+        sort(graph[i].begin(),graph[i].end());
+    }
+    bfs(r);
+    for(int i = 1; i<=n; i++){
+        cout << isVisited[i] << '\n';
+    }
+}

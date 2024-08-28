81438177fe4308cc2be6aacde7acafd924a5382e
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2024-08-28 11:38:20 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    long long a, b, c, ans;
+    cin >> a >> b >> c;
+    ans = a+b+c;
+    cout << ans;
+}

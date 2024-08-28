4a150ded15042f6128a6ca2f00799fda85b332ec
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2024-08-28 11:39:09 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 524 ms, Memory: 212172 KB -BaekjoonHub

---
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <set>
+using namespace std;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    set<string> strset;
+    string str;
+    cin >> str;
+    int strlen = str.length();
+    for(int i = 0; i < strlen; i++){
+        for(int j = 0; j<strlen-i; j++){
+            string partstr = str.substr(i,j+1);
+            strset.insert(partstr);
+            
+        }
+    }
+    cout << strset.size();
+
+}

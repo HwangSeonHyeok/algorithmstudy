5e24d920fb2462f2e00b4580c4f2ec9eeb4fc2a7
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2024-08-28 10:51:07 +0900

    [Silver IV] Title: 대칭 차집합, Time: 224 ms, Memory: 20544 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <unordered_set>
+#include <string>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    unordered_set<int> A,B;
+    int a, b, cnt = 0;
+    cin >> a >> b;
+    for(int i = 0; i<a; i++){
+        int num;
+        cin >> num;
+        A.insert(num);
+    }
+    for(int i = 0; i<b; i++){
+        int num;
+        cin >> num;
+        if(A.find(num)==A.end()){
+            B.insert(num);
+        }else{
+            cnt++;
+        }
+    }
+    int num = A.size() + B.size() - cnt;
+    cout << num;
+
+}

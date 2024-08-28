5e4216bed7cdfbdb98ddc1810b92341f82fbb904
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2024-08-28 10:46:17 +0900

    [Silver III] Title: 어린 왕자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >>t;
+    for(int i = 0; i<t;i++){
+        int x1, y1, x2, y2, n, cnt = 0;
+        cin >> x1 >> y1 >> x2 >> y2;
+        cin >> n;
+        for(int j = 0; j<n; j++){
+            int cx, cy, r;
+            cin >> cx >> cy >> r;
+            if((pow(r,2)-pow(cx-x1,2)-pow(cy-y1,2))*((pow(r,2)-pow(cx-x2,2)-pow(cy-y2,2)))<0){
+                cnt++;
+            }
+        }
+        cout << cnt <<'\n';
+    }
+}

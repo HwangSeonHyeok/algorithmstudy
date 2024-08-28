bdc34ac48e99a83f84512e35c155ae554a51182d
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2024-08-28 11:40:41 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int dp[1001] = {0,};
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int n;
+    cin >> n;
+    dp[0] = 1;
+    dp[1] = 2;
+    for(int i = 2; i<n; i++){
+        dp[i] = (dp[i-1]+ dp[i-2])%10007;
+    }
+    cout << dp[n-1];
+
+
+}
+

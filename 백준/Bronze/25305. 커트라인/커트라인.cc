f51cc5cf782556f7c90a4f0d23744250f4a1b23b
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2024-08-28 11:58:28 +0900

    [Bronze II] Title: 커트라인, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int n, k,score[1010];
+    cin >> n >> k;
+    for(int i = 0; i<n; i++){
+        int x;
+        cin >> x;
+        score[i] = x;
+    }
+    sort(score, score+n);
+    cout << score[n-k];
+}

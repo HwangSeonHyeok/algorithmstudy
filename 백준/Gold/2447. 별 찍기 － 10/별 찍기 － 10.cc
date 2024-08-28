24d291769195242ab5080bf001b9389593f4fe63
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2024-08-28 11:04:02 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 184 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+void star(int n, int i, int j){
+    if((i/n)%3 == 1 && (j/n) % 3 ==1){
+        printf(" ");
+    }
+    else{
+        if(n/3 == 0) printf("*");
+        else star(n/3,i,j);
+    }
+    
+}
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            star(n, i,j);
+        }
+        printf("\n");
+    }
+
+}

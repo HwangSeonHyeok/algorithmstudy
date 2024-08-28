606dfdfaaf067a9ca8890e95ae68e390957a68ca
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2024-08-28 11:11:13 +0900

    [Bronze III] Title: 택시 기하학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.cc
@@ -0,0 +1,16 @@
+#define _USE_MATH_DEFINES
+#include<iostream>
+#include<math.h>
+using namespace std; 
+
+int main(){
+    double r, e, t; 
+    scanf("%lf", &r);
+    e = r*r * M_PI;
+    printf("%lf\n", e);
+    t = r*r*2;
+    printf("%lf\n", t);
+    
+
+
+}

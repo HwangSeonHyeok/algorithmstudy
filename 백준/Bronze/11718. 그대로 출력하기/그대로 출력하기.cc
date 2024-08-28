0b9b60754ef8794cee7e755677ec673954ae736f
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2024-08-28 11:40:05 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main(){
+    // ios::sync_with_stdio(false);
+    // cin.tie(0);
+    // cout.tie(0);
+    
+    while(1){
+        string s;
+        getline(cin,s);
+        if(s=="") break;
+        cout << s <<'\n';
+    }
+    
+}

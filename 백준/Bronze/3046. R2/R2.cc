8c37b4a409b2ce99c7ed1ea63a3e23217c71d24e
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2024-08-28 11:10:55 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    int R1, R2, S;
+    cin >> R1 >> S;
+    R2 = (S*2)-R1;
+    cout << R2;
+}

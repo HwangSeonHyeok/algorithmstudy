9402434e8ae759bfb972db94945ab75d7098f109
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2024-08-28 11:18:00 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std; 
+int main()
+{
+    int score, sum = 0;
+    for(int i = 0; i < 5; i++){
+        cin >> score;
+        sum += score;
+    }
+    cout << sum; 
+}

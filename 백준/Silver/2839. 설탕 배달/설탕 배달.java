fad1c00580d0061f1fac5710b28854555d5bc357
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2024-09-03 14:22:29 +0900

    [Silver IV] Title: 설탕 배달, Time: 100 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -7,18 +7,18 @@ public class Main {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(in.readLine());
 		int ans = 0;
-		if(n==4||n==7) {
+		if (n == 4 || n == 7) {
 			ans = -1;
-		}else if(n%10==1||n%10==6) {
-			ans = (n/5-1)+2;
-		}else if(n%10==2||n%10==7) {
-			ans = (n/5-2)+4;
-		}else if(n%10==3||n%10==8) {
-			ans = (n/5)+1;
-		}else if(n%10==4||n%10==9) {
-			ans = (n/5-1)+3;
-		}else if(n%10==5||n%10==0) {
-			ans = (n/5);
+		} else if (n % 5 == 0) {
+			ans = (n / 5);
+		} else if (n % 5 == 1) {
+			ans = (n / 5 - 1) + 2;
+		} else if (n % 5 == 2) {
+			ans = (n / 5 - 2) + 4;
+		} else if (n % 5 == 3) {
+			ans = (n / 5) + 1;
+		} else if (n % 5 == 4) {
+			ans = (n / 5 - 1) + 3;
 		}
 		System.out.println(ans);
 	}

eebbd3349c853ef7c819f25a6bae675c4a5dfe23
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2024-09-09 14:10:19 +0900

    [Gold IV] Title: 수 나누기 게임, Time: 408 ms, Memory: 38520 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.java
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+	static int[] existCard = new int[1000001];
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -11,30 +12,23 @@ public class Main {
 		int n = Integer.parseInt(in.readLine());
 		String[] split = in.readLine().split(" ");
 		int[] cardArr = new int[n];
-		
 		int[] score = new int[n];
 		int max = 0;
 		for (int i = 0; i < n; i++) {
 			cardArr[i] = Integer.parseInt(split[i]);
+			existCard[cardArr[i]] = i+1;
 			if(max<cardArr[i]) {
 				max = cardArr[i];
 			}
 		}
-		List<Integer>[]dList  = new ArrayList[max+1];
-		for(int i = 0; i<max+1;i++) {
-			dList[i] = new ArrayList<>();
-		}
 		for(int i = 0; i<n;i++) {
 			int current = cardArr[i]*2;
 			while(current<=max) {
-				dList[current].add(i);
+				if(existCard[current]>0) {
+					score[existCard[current]-1]--;
+					score[i]++;
+				}
 				current += cardArr[i];
-			}
-		}
-		for(int i = 0; i<n;i++) {
-			score[i]-=dList[cardArr[i]].size();
-			for(int idx :dList[cardArr[i]]) {
-				score[idx]++;
 			}
 		}
 		for(int s :score) {

84da88c25ff06017e06e55654044a1c4bff0c5b0
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2024-11-10 18:04:13 +0900

    [Gold V] Title: 4와 7, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Gold/2877. 4와 7/4와 7.java
+++ 백준/Gold/2877. 4와 7/4와 7.java
@@ -5,8 +5,8 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(in.readLine()) + 1;
-		String binary = Integer.toString(n, 2);
+		int n = Integer.parseInt(in.readLine());
+		String binary = Integer.toString(n+1, 2);
 		String ans = "";
 		for (int i = 1; i < binary.length(); i++) {
 			if (binary.charAt(i) == '0') {

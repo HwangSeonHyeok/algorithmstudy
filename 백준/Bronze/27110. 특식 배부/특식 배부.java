9e56c4752e983c5f3fd19cc06f6c9fa2856f9a6d
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2024-11-27 01:26:25 +0900

    [Bronze IV] Title: 특식 배부, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/27110. 특식 배부/특식 배부.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(in.readLine());
+		String[] split = in.readLine().split(" ");
+		int ans = 0;
+		for (int i = 0; i < 3; i++) {
+			ans += Math.min(Integer.parseInt(split[i]), n);
+		}
+		System.out.println(ans);
+
+	}
+
+}

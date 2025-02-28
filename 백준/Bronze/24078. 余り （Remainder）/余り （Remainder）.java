2d89e8531bd34a4c16a2c76739d6c0476d040033
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2025-03-01 01:17:41 +0900

    [Bronze V] Title: 余り (Remainder), Time: 104 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/24078. 余り （Remainder）/余り （Remainder）.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int x = Integer.parseInt(in.readLine());
+		System.out.println(x % 21);
+	}
+
+}

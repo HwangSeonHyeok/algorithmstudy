4ef9a25549a85163ff6e233716d06b0178738154
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2025-03-01 23:02:33 +0900

    [Bronze V] Title: 火曜日 (Tuesday), Time: 104 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/31611. 火曜日 （Tuesday）/火曜日 （Tuesday）.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int x = Integer.parseInt(in.readLine());
+		System.out.println(x % 7 == 2 ? 1 : 0);
+
+	}
+
+}

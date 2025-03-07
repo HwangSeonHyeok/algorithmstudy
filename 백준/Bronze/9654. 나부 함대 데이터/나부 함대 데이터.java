5c25743dda7b716c589514b0822aced4165ed8c4
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2025-03-08 02:45:47 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 100 ms, Memory: 13908 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println(
+				"SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\nN2 Bomber      Heavy Fighter  Limited    21        \nJ-Type 327     Light Combat   Unlimited  1         \nNX Cruiser     Medium Fighter Limited    18        \nN1 Starfighter Medium Fighter Unlimited  25        \nRoyal Cruiser  Light Combat   Limited    4         ");
+	}
+
+}

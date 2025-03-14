0922e59f76f69a14facdf04a15f6121c3304d837
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2025-03-15 01:34:27 +0900

    [Silver IV] Title: 떡국, Time: 356 ms, Memory: 60312 KB -BaekjoonHub

--- 백준/Silver/20937. 떡국/떡국.java
+++ 백준/Silver/20937. 떡국/떡국.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 
@@ -8,22 +7,14 @@ public class Main {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(in.readLine());
 		String[] split = in.readLine().split(" ");
-		int[] arr = new int[n];
+		int[] arr = new int[50001];
 		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(split[i]);
+			arr[Integer.parseInt(split[i])]++;
 		}
-		Arrays.sort(arr);
-		int max = 1;
-		int count = 1;
-		for (int i = 1; i < n; i++) {
-			if (arr[i] == arr[i - 1]) {
-				count++;
-			} else {
-				max = Math.max(max, count);
-				count = 1;
-			}
+		int max = 0;
+		for (int i = 1; i <= 50000; i++) {
+			max = Math.max(max, arr[i]);
 		}
-		max = Math.max(max, count);
 		System.out.println(max);
 
 	}

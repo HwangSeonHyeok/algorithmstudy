ac73137a04f8f46689770f0211283ab927f92a92
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2025-10-02 01:46:15 +0900

    [Bronze V] Title: Class Photo, Time: 96 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/34281. Class Photo/Class Photo.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(in.readLine());
+		int m = Integer.parseInt(in.readLine());
+		System.out.println(n*m);
+
+	}
+
+}

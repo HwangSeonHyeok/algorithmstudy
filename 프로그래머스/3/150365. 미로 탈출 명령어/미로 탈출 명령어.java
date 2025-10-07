559f74b61789e0d0032492088d6da8baa2186ae6
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2025-10-07 20:49:22 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 17.18 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class Solution {
+    int[] dx = {1,0,0,-1};
+    int[] dy = {0,-1,1,0};
+    char[] direction = {'d','l','r','u'};
+    boolean isInRange(int n, int m, int x, int y){
+        return x>0&&y>0&&x<=n&&y<=m;
+    }
+    char[] route;
+    boolean dfs(int n, int m, int x, int y, int r, int c, int k, int depth){
+        int remainDist = Math.abs(r-x)+Math.abs(c-y);
+        if(remainDist>k-depth)return false;
+        if((k-depth)%2!=remainDist%2)return false;     
+        if(x==r && y==c && depth==k){
+            return true;
+        }
+        if(depth==k){
+            return false;
+        }
+        for(int i = 0;i<4;i++){
+            int nextX = x+dx[i];
+            int nextY = y+dy[i];
+            if(isInRange(n,m,nextX,nextY)){
+                route[depth] = direction[i];
+                if(dfs(n, m, nextX, nextY, r, c, k,depth+1)){
+                    return true;
+                }
+            }
+            
+        }
+        return false;
+    }
+    public String solution(int n, int m, int x, int y, int r, int c, int k) {
+        route = new char[k];
+        String answer = "";
+        if(dfs(n,m,x,y,r,c,k,0)){
+            answer = new String(route);
+        }else{
+            answer = "impossible";
+        }
+        return answer;
+    }
+}

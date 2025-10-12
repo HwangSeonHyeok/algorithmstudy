9e02f380ade181a30c5047bbd496d5eca93010ee
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2025-10-13 02:44:54 +0900

    [Bronze V] Title: SciComLove, Time: 100 ms, Memory: 13964 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(in.readLine());
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < n; i++) {
+			sb.append("SciComLove\n");
+		}
+		System.out.println(sb);
+
+	}
+
+}

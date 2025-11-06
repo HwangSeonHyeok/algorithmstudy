4628ceb7d87a45d1ce644ff7682fa95643fca0cd
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2025-11-07 03:55:56 +0900

    [Bronze V] Title: Zadanie próbne 2, Time: 116 ms, Memory: 15888 KB -BaekjoonHub

---
+++ 백준/Bronze/8871. Zadanie próbne 2/Zadanie próbne 2.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(in.readLine()) + 1;
+		System.out.println(n * 2 + " " + n * 3);
+
+	}
+
+}

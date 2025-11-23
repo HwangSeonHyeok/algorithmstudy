1dc77f48a7e6dd25d0ca4e9a25b9f7766b16d531
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2025-11-24 00:34:18 +0900

    [Silver V] Title: 팔찌 만들기, Time: 108 ms, Memory: 14528 KB -BaekjoonHub

--- 백준/Silver/25707. 팔찌 만들기/팔찌 만들기.java
+++ 백준/Silver/25707. 팔찌 만들기/팔찌 만들기.java
@@ -16,7 +16,7 @@ public class Main {
 		Arrays.sort(arr);
 		int ans = 0;
 		for (int i = 0; i < n - 1; i++) {
-			ans += arr[(i + 1) % n] - arr[i];
+			ans += arr[i + 1] - arr[i];
 		}
 		ans += arr[n - 1] - arr[0];
 		System.out.println(ans);

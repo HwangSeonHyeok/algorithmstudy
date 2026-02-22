3b2fe03cb4d91c8d2987d809e2f76d886b787397
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2026-02-22 17:11:39 +0900

    [Bronze I] Title: Fractions are better when continued, Time: 96 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/32437. Fractions are better when continued/Fractions are better when continued.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		long[] fibo = new long[41];
+		fibo[0] = 1;
+		fibo[1] = 1;
+		for (int i = 2; i < fibo.length; i++) {
+			fibo[i] = fibo[i - 1] + fibo[i - 2];
+		}
+		int n = Integer.parseInt(in.readLine());
+
+		System.out.println(fibo[n]);
+
+	}
+
+}
